6afc8a112a8d03ab0cce0861d11a6bf0a1027c48
Jeongbin Kim <dev@menumap.co.kr>
2015-06-16 23:55:29 +0900

    Add git configuration and AWS S3 variables

--- app_environment_variables.rb
+++ app_environment_variables.rb
@@ -6,18 +6,22 @@ ENV['RDS_PORT']     = '3306'
 
 if Rails.env.development?
   ENV['RDS_DB_NAME']  = 'new_menumap_development'
-end
-
-if Rails.env.production?
+elsif Rails.env.production?
   ENV['RDS_DB_NAME']  = 'new_menumap_production'
-end
-
-if Rails.env.test?
+else Rails.env.test?
   ENV['RDS_DB_NAME']  = 'new_menumap_test'
 end
 
 # S3
 ENV['S3_ACCESS_KEY'] = 'AKIAJZ2F3JYQPCPK76DQ'
 ENV['S3_SECRET_KEY'] = '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'
-ENV['S3_BUCKET'] = 'menumaps3'
 ENV['S3_REGION'] = 'ap-northeast-1'
+
+if Rails.env.development?
+	ENV['S3_BUCKET'] = 'menumap-s3-development'
+elsif Rails.env.production?
+	ENV['S3_BUCKET'] = 'menumap-s3-production'
+else Rails.env.test?
+	# pending creation
+	ENV['S3_BUCKET'] = 'menumap-s3-test'
+end
