49664a0819be9fc9762c011993073f478de5ec10
dev_menumap <dev@menumap.co.kr>
2015-06-16 23:38:56 +0900

    Add S3 variables

--- app_environment_variables.rb
+++ app_environment_variables.rb
@@ -15,3 +15,9 @@ end
 if Rails.env.test?
   ENV['RDS_DB_NAME']  = 'new_menumap_test'
 end
+
+# S3
+ENV['S3_ACCESS_KEY'] = 'AKIAJZ2F3JYQPCPK76DQ'
+ENV['S3_SECRET_KEY'] = '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'
+ENV['S3_BUCKET'] = 'menumaps3'
+ENV['S3_REGION'] = 'ap-northeast-1'
