4819b95407086cf347b5e7c619f55c15b9819eab
Jeongbin Kim <dev@menumap.co.kr>
2015-07-22 21:13:12 +0900

    gogo

--- environment_variables.rb
+++ environment_variables.rb
@@ -26,3 +26,10 @@ end
 
 # Devise
 ENV['DEVISE_SECRET_KEY'] = '660fca6110afbe041b243dc2a314d7e66820df093ca44a0b889162475bd3b8d32fa9ad7a211671d3f17bcf582244966a0d47d5a2ed27e186c62646cad28955a3'
+
+# SES
+ENV['SENDER'] = 'info@menumap.co.kr'
+ENV['SES_ADDRESS'] = 'email-smtp.us-east-1.amazonaws.com'
+ENV['SES_PORT'] = '587'
+ENV['SES_USER_NAME'] = 'AKIAJOQLGTLT7IVGM5KQ'
+ENV['SES_PASSWORD'] = 'Ag7hdXlaO5DzdsfCEzk/iRw4RdN4E1UJkiH3aI6jeqsv'
