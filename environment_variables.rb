21de5769d62bc4aee52d685e1e74aa39764e268f
Jeongbin Kim <dev@menumap.co.kr>
2015-08-13 13:39:13 +0900

    facebook

--- environment_variables.rb
+++ environment_variables.rb
@@ -27,6 +27,10 @@ end
 # Devise
 ENV['DEVISE_SECRET_KEY'] = '660fca6110afbe041b243dc2a314d7e66820df093ca44a0b889162475bd3b8d32fa9ad7a211671d3f17bcf582244966a0d47d5a2ed27e186c62646cad28955a3'
 
+# Facebook
+ENV['FB_APP_ID'] = '1053593421318380'
+ENV['FB_APP_SECRET'] = '25db942c04bb6e4b69c29cd2982a01d9'
+
 # SES
 ENV['SENDER'] = 'info@menumap.co.kr'
 ENV['SES_ADDRESS'] = 'email-smtp.us-east-1.amazonaws.com'
